4b23ba8c267de299081e97f80d1c36a1c12080d9
Luis Thomas <luis.thomas2005@gmail.com>
2020-11-24 09:59:06 +0100

    install flatpaks

--- setup.sh
+++ setup.sh
@@ -7,12 +7,6 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm 
 # JetBrains Toolbox
 wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-1.18.7609.tar.gz && tar xf jetbrains-toolbox-1.18.7609.tar.gz && ./jetbrains-toolbox-1.18.7609/jetbrains-toolbox
 
-# Change keybindings for switch-applications and for switch-windows
-gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
-gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
-
 # Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
